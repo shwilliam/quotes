dbff9bb03ec4515f13aecd6a5ca85ae48a82b0c6
William L <w-lindvall@outlook.com>
2019-07-30 20:45:14 -0700

    hide submit form if non auth user

--- page-submit.php
+++ page-submit.php
@@ -11,50 +11,59 @@ get_header(); ?>
     <?= the_title(); ?>
   </h2>
 
-  <form id="quote__form" action="">
-    <label for="quote__author">
-      Author of Quote
-      <input
-        name="quote__author"
-        id="quote__author"
-        class="input"
-        type="text"
-        required
-      >
-    </label>
+  <?php if (is_user_logged_in()) : ?>
 
-    <label for="quote__content">
-      Quote
-      <textarea
-        name="quote__content"
-        id="quote__content"
-        class="input"
-        rows="3"
-        required
-      ></textarea>
-    </label>
+    <form id="quote__form" action="">
+      <label for="quote__author">
+        Author of Quote
+        <input
+          name="quote__author"
+          id="quote__author"
+          class="input"
+          type="text"
+          required
+        >
+      </label>
 
-    <label for="quote__source">
-      Where did you find this quote? (e.g. book name) 
-      <input
-        name="quote__source"
-        id="quote__source"
-        class="input"
-        type="text"
-      >
-    </label>
+      <label for="quote__content">
+        Quote
+        <textarea
+          name="quote__content"
+          id="quote__content"
+          class="input"
+          rows="3"
+          required
+        ></textarea>
+      </label>
 
-    <label for="quote__url">
-      Provide the URL of the quote source, if available
-      <input
-        name="quote__url"
-        id="quote__url"
-        class="input"
-        type="url"
-      >
-    </label>
+      <label for="quote__source">
+        Where did you find this quote? (e.g. book name) 
+        <input
+          name="quote__source"
+          id="quote__source"
+          class="input"
+          type="text"
+        >
+      </label>
 
-    <button class="button" type="submit">WOO</input>
-  </form>
+      <label for="quote__url">
+        Provide the URL of the quote source, if available
+        <input
+          name="quote__url"
+          id="quote__url"
+          class="input"
+          type="url"
+        >
+      </label>
+
+      <button class="button" type="submit">WOO</input>
+    </form>
+
+  <?php else : ?>
+
+    <p>Sorry, you must be logged in to submit a quote!</p>
+    <a href="<?= esc_url(admin_url()); ?>">Click here to login.</a>
+
+  <?php endif; ?>
 
 <?php get_footer(); ?>
