599d50e59444f75459990e93516ca912df96becd
William L <w-lindvall@outlook.com>
2019-08-02 23:52:36 -0700

    handle empty archives queries

--- page-archives.php
+++ page-archives.php
@@ -7,46 +7,46 @@
 
 get_header(); ?>
 
-  <!-- TODO: handle empty lists -->
-
   <h2>
     <?= the_title(); ?>
   </h2>
 
-  <section>
-    <h3>Quote Authors</h3>
+  <?php
+    $posts = get_posts(array(
+      'posts_per_page' => '-1'
+    ));
 
-    <ul class="list list--inline list--inline-padded">
-      <?php
-        $posts = get_posts(array(
-          'posts_per_page' => '-1'
-        ));
-        foreach ($posts as $post) : setup_postdata($post);
-      ?>
+    if ($posts) : ?>
+      <section>
+        <h3>Quote Authors</h3>
 
-        <li>
-          <a href="<?= esc_url(get_permalink()); ?>">
-            <?= the_title(); ?>
-          </a>
-        </li>
+        <ul class="list list--inline list--inline-padded">
+        <?php foreach ($posts as $post) : setup_postdata($post); ?>
 
-      <?php
-        endforeach; wp_reset_postdata();
-      ?>
-    </ul>
-  </section>
+          <li>
+            <a href="<?= esc_url(get_permalink()); ?>">
+              <?= the_title(); ?>
+            </a>
+          </li>
 
-  <section>
-    <h3>Categories</h3>
+        <?php endforeach; wp_reset_postdata(); ?>
+      </ul>
+    </section>
+  <?php endif; ?>
 
-    <ul class="list list--inline list--inline-padded">
-      <?=
-        wp_list_categories(array(
-          'title_li' => '',
-        ));
-      ?>
-    </ul>
-  </section>
+  <?php if (wp_count_posts() > 0) : ?>
+    <section>
+      <h3>Categories</h3>
+
+      <ul class="list list--inline list--inline-padded">
+        <?=
+          wp_list_categories(array(
+            'title_li' => '',
+          ));
+        ?>
+      </ul>
+    </section>
+  <?php endif; ?>
 
   <?php $tags = get_tags(); if ($tags) : ?>
     <section>
